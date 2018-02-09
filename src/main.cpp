a645a15a3bcdfa4d73ae772912841a9f96cdf6f9
Lars Flæten <lars.flaeten@gmail.com>
2018-02-09 07:58:23 +0100

    Example usage

--- src/main.cpp
+++ src/main.cpp
@@ -8,78 +8,29 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    int suceeded = 0;
-    int failed = 0;
-    std::string test;
     std::shared_ptr<Datalogger> dl;
 
 
-    test = "Initialization";
-    try {
-        dl = std::make_shared<Datalogger>("blackbox.dat");
+    dl = std::make_shared<Datalogger>("blackbox.dat");
     
-        cout << "[TEST] " << test << ": PASSED" << endl;
-        suceeded++;
-    } catch (...)
+    dl->registerValueName("TIME", "jd", "DOUBLE");
+    dl->registerValueName("POS", "x", "DOUBLE");
+    dl->registerValueName("POS", "y", "DOUBLE");
+    dl->registerValueName("INTS", "(Step*2)+3", "INT");
+
+    int i = 0; 
+    for(double t = 0.0; t < 400.0; t += 0.1)
     {
-        cout << "[TEST] " << test << ": FAILED" << endl;
-        failed++;
-    } 
+        dl->tick(t);
 
-    test = "Try multiple value&type registration";
-    try{
-	    dl->registerValueName("TIME", "jd", "DOUBLE");
-   		dl->registerValueName("POS", "x", "DOUBLE");
-    	dl->registerValueName("POS", "y", "DOUBLE");
-    	dl->registerValueName("POS", "z", "DOUBLE");    
-    	dl->registerValueName("MASS", "m", "DOUBLE");
-    	dl->registerValueName("AP_STATE", "state", "INT");
+        dl->writeValue("TIME", "jd", 215000.5+t/60.0/60.0/24.0);
+        dl->writeValue("POS", "x" , -500.0 + t);
+        dl->writeValue("POS", "y" , -100.0 + t/50.0);
+        dl->writeValue("INTS", "(Step*2)+3", i*2+3);
 
-    	cout << "[TEST] " << test << ": PASSED" << endl;
-        suceeded++;
-    } catch (...)
-    {
-        cout << "[TEST] " << test << ": FAILED" << endl;
-        failed++;
+        dl->tock();
+        i++;
     }
-
-	test = "Try wrong type registration";
-    try{
-        dl->registerValueName("SOMECLASS", "somevalue", "_wrong_type_");
-        
-        cout << "[TEST] " << test << ": FAILED" << endl;
-        failed++;
-    } catch (...)
-    { 
-        cout << "[TEST] " << test << ": PASSED" << endl;
-        suceeded++;
-    }
-
-	test = "Try empty class registration";
-    try{
-        dl->registerValueName("", "somevalue", "INT");
-
-        cout << "[TEST] " << test << ": FAILED" << endl;
-        failed++;
-    } catch (...)
-    {
-        cout << "[TEST] " << test << ": PASSED" << endl;
-        suceeded++;
-    }
-
-	test = "Try empty value registration";
-    try{
-        dl->registerValueName("SOMECLASS", "", "DOUBLE");
-
-        cout << "[TEST] " << test << ": FAILED" << endl;
-        failed++;
-    } catch (...)
-    {
-        cout << "[TEST] " << test << ": PASSED" << endl;
-        suceeded++;
-    }
-
-
 
 
 
