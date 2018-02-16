9c626e3955665c4804b9e8e84d744d5c9b532b2e
Lars Flæten <lars.flaeten@gmail.com>
2018-02-16 23:57:34 +0100

    Just testing with different values

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <iostream>
+#include <cmath>
 
 #include "Datalogger.h"
 
@@ -11,7 +12,7 @@ int main(int argc, char** argv) {
     std::shared_ptr<Datalogger> dl;
 
 
-    dl = std::make_shared<Datalogger>("blackbox.dat");
+    dl = std::make_shared<Datalogger>("blackbox.dat", 2.0);
     
     dl->registerValueName("TIME", "jd", "DOUBLE");
     dl->registerValueName("POS", "x", "DOUBLE");
@@ -19,12 +20,12 @@ int main(int argc, char** argv) {
     dl->registerValueName("INTS", "(Step*2)+3", "INT");
 
     int i = 0; 
-    for(double t = 0.0; t < 400.0; t += 0.1)
+    for(double t = 0.0; t < 40.0; t += 0.01)
     {
         dl->tick(t);
 
         dl->writeValue("TIME", "jd", 215000.5+t/60.0/60.0/24.0);
-        dl->writeValue("POS", "x" , -500.0 + t);
+        dl->writeValue("POS", "x" , 20.0*sin(t/20.0));
         dl->writeValue("POS", "y" , -100.0 + t/50.0);
         dl->writeValue("INTS", "(Step*2)+3", i*2+3);
 
